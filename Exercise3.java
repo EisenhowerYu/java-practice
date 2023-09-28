9d6a4f1909c1cb9a607852c6274445044d25eea3
Eisen <eisenhowerkyu@gmail.com>
2023-09-28 17:02:51 +0800

    Average calculator

--- Exercise3.java
+++ Exercise3.java
@@ -2,14 +2,27 @@ import java.util.Scanner;
 
 public class Exercise3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        Double avg;
-        
-        System.out.println("Enter numbers. Enter x to stop.");
-        Double num = Double.valueOf(scanner.nextLine());
 
-        System.out.println("Average: " + num);
+        System.out.println("Enter numbers. Enter x to stop.");
+        Double avg = 0.0;
+        int denom = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("x")) {
+                break;
+            } else {
+                Double num = Double.valueOf(input);
+                avg = avg + num;
+                denom++;
+            }
+        }
+
+        avg = avg / denom;
+
+        System.out.println("Average: " + avg);
 
         scanner.close();
     }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # java-practice
+
+Language: Java
+
+## Description
+
+Exercises from the online MOOC Java Programming I and Java Programming II by University of Helsinki. 
+The course covers the basics of computer programming, along with algorithms and object-oriented programming through the Java programming language.
