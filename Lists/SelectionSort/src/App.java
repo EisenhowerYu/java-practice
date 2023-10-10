94a7779651c7debd546d35e5f68693664f8fedbe
Eisen <eisenhowerkyu@gmail.com>
2023-10-10 16:34:29 +0800

    binary search

--- Lists/SelectionSort/src/App.java
+++ Lists/SelectionSort/src/App.java
@@ -1,27 +1,69 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] arr1 = {3, 2, 9, 1, 8};
+        int[] arr1 = {4, 2, 9, 1, 8};
+        arr1 = bubbleSort(arr1);
+        for (int i: arr1) {
+            System.out.println(i);
+        }
+        //System.out.println(linearSearch(arr1, 9));
+        System.out.println(binarySearch(arr1, 8));
+    }
 
+    public static int[] selectionSort(int[] arr) {
         int tmp;
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = i; j < arr1.length; j++) {
-                if (arr1[i] > arr1[j]) {
-                    tmp = arr1[i];
-                    arr1[i] = arr1[j];
-                    arr1[j] = tmp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
                 }
             }
         }
 
-        for (int i: arr1) {
-            System.out.println(i+" ");
-        }
+        return arr;
     }
 
-    // public static int selectionSort(ArrayList<Integer> list) {
-    //     for (int i = 0; i < list.size(); i++) {
-    //         findMin(list);
-    //     }
-    //     return 0;
-    // }
+    public static int[] bubbleSort(int[] arr) {
+        boolean sorted = false;
+        int tmp;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i+1]) {
+                    tmp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = tmp;
+                    sorted = false;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static int linearSearch(int[] arr, int key) {
+        int idx = -1; // not found
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                idx = i;
+                break;
+            }
+        }
+        return idx;
+    }
+
+    public static int binarySearch(int[] arr, int key) {
+        int mid = arr.length / 2;
+        int len = arr.length + mid;
+        if (key > arr[mid]) {
+            return binarySearch(Arrays.copyOfRange(arr, mid, arr.length), key);
+        } else if (key < arr[mid]) {
+            return binarySearch(Arrays.copyOfRange(arr, 0, mid), key);
+        } else {
+            return len + mid;
+        }
+    }
 }
