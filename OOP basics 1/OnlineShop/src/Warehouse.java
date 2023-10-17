f5c172597c2bd3e9a7c5ea826e0b34f447cdcda9
Eisen <eisenhowerkyu@gmail.com>
2023-10-17 15:00:28 +0800

    basic set implementation

--- OOP basics 1/OnlineShop/src/Warehouse.java
+++ OOP basics 1/OnlineShop/src/Warehouse.java
@@ -1,22 +1,41 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Warehouse {
     private Map<String, Integer> mapPrice;
+    private Map<String, Integer> mapStock;
 
     public Warehouse() {
         this.mapPrice = new HashMap<>();
+        this.mapStock = new HashMap<>();
     }
 
     public void addProduct(String product, int price, int stock) {
         mapPrice.put(product, price);
+        mapStock.put(product, stock);
+    }
+
+    public boolean take(String product) {
+        if (mapStock.get(product) == null || mapStock.get(product) < 1) {return false;}
+        mapStock.put(product, mapStock.get(product) - 1);
+        return true;
     }
 
     public int price(String product) {
-        if (mapPrice.get(product) == null) {
-            return -99;
+        if (mapPrice.get(product) == null) {return -99;}
+        return mapPrice.get(product);
+    }
+
+    public int stock(String product) {
+        if (mapStock.get(product) == null) {
+            return 0;
         } else {
-            return mapPrice.get(product);
+            return mapStock.get(product);
         }
+    }
+
+    public Set<String> products() {
+        return mapStock.keySet();
     }
 }
